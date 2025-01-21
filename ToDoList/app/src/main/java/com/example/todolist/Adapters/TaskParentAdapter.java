32d69ddb1b2c7e38e91e82e1fe94583c28645bc9
Tung <junbuvan2009@gmail.com>
2025-01-21 17:53:37 +0700

    - Modify task parent layout

--- ToDoList/app/src/main/java/com/example/todolist/Adapters/TaskParentAdapter.java
+++ ToDoList/app/src/main/java/com/example/todolist/Adapters/TaskParentAdapter.java
@@ -3,22 +3,17 @@ package com.example.todolist.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.todolist.MainActivity;
-import com.example.todolist.Models.ToDoModel;
 import com.example.todolist.R;
 import com.example.todolist.ToDoListActivity;
-import com.example.todolist.Utils.DataBaseHandler;
 import com.example.todolist.Utils.ToDoParentList;
 
 import java.util.List;
@@ -50,7 +45,7 @@ public class TaskParentAdapter extends RecyclerView.Adapter<TaskParentAdapter.Vi
                 mainActivity.startActivity(intent);
             }
         });
-        holder.taskButton.setText(item.Text);
+        holder.taskText.setText(item.Text);
     }
     public int getItemCount()
     {
@@ -67,10 +62,14 @@ public class TaskParentAdapter extends RecyclerView.Adapter<TaskParentAdapter.Vi
     public static class  ViewHolder extends RecyclerView.ViewHolder
     {
         Button taskButton;
+        TextView taskText;
+        TextView taskModifyDateText;
         ViewHolder(View view)
         {
             super(view);
-            taskButton = view.findViewById(R.id.taskParent);
+            taskButton = view.findViewById(R.id.taskParentButton);
+            taskText = view.findViewById(R.id.taskParentText);
+            taskModifyDateText = view.findViewById(R.id.taskModifyDateText);
         }
     }
 }
